c42b4d8581302466b58021ba88abeb2d02e440ef
Yun Jongwon <dmdmdkdk@daum.net>
2018-12-18 09:37:24 +0900

    second commit

--- req.py
+++ req.py
@@ -4,8 +4,7 @@ import bs4
 response = requests.get('https://finance.naver.com/marketindex/').text
 soup = bs4.BeautifulSoup(response,'html.parser')
 result = soup.select_one('#exchangeList > li.on > a.head.usd > div > span.value').text
-a= soup.select_one('#exchangeList > li.on > a.head.usd > div > span.value').text
 
 print('현재 원/달러 환율은 '+ result + '입니다.')
-print(f'현재 원/달러의 환율은  {result} 입니다.')
-print('현재 원/달러의 환율은  {} 입니다.{}'.format(result,a))
+
+print('현재 원/달러 환율은  {} 입니다.'.format(result))
